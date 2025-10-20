7cadd4038cdf9c8ffe097fb0b3e019e7c3c991af
Rezharivai <ffairuzsadidah@gmail.com>
2025-10-20 10:38:57 +0700

    ubah isi Main

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input dua angka: ");
+        int inp1 = sc.nextInt();
+        System.out.println("Input dua angka: ");
+        int inp2 = sc.nextInt();
+
+        int hasil = inp1 + inp2;
+
+        System.out.println("Penjumlahan " + inp1 + " + " + inp2 + " = " + hasil);
     }
 }
