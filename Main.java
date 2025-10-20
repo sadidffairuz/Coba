d5a041740d0d7352611c44d7a9a161a080dcea6b
Rezharivai <ffairuzsadidah@gmail.com>
2025-10-20 11:12:56 +0700

    ubah README lagi

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input dua angka: ");
+        System.out.println("Input angka pertama: ");
         int inp1 = sc.nextInt();
-        System.out.println("Input dua angka: ");
+        System.out.println("Input angka kedua: ");
         int inp2 = sc.nextInt();
 
         int hasil = inp1 + inp2;
 
+        System.out.println("angka 1: " + inp1);
+        System.out.println("angka 2: " + inp2);
         System.out.println("Penjumlahan " + inp1 + " + " + inp2 + " = " + hasil);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Coba
+program penjumlahan sederhana
+user diminta untuk menginput 2 bilangan bulat
+program akan menjumlahkan bilangan yang diinput oleh user
+selanjutnya program akan menampilkan hasil oenjumlahan dari bilangan yang diinputkan user
+
